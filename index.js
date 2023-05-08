86b221bde163eead23056950b7f57432824ee044
Malique Hammonds <107649903+Leekasaurus@users.noreply.github.com>
2023-05-08 10:12:03 -0400

    added more arrays for the homework

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 const coffeeMenu = require("./coffee_data");
 
+
+// Return an array of all the drinks on the menu.
+
+const drinks = coffeeMenu.filter((element)); {
+    return characters.name.includes();
+};
+console.log(name);
+
+
+// an array of all the items that are less or equal to $5
+const underFive = (item) => {
+    return item.price <= 5;
+}
+const itemsUnderFive = coffeeMenu.filter(underFive);
+console.table(itemsUnderFive);
+
+
+//Return an array of drinks that are priced at an even number.
+const evenArr = coffeeMenu.filter(function (num) {
+    return num % 2 === 0;
+});
+console.log(evensArr);
